1ea2ac64cde8dcdc5b40555cd859f91c1d0f8289
Huang56 <635837244@qq.com>
2019-04-24 19:53:45 +0800

    提取Star静态组件[C

--- src/components/ShopList/ShopList.vue
+++ src/components/ShopList/ShopList.vue
@@ -20,11 +20,7 @@
             <section class="shop_rating_order">
               <section class="shop_rating_order_left">
                 <div class="star star-24">
-                  <span class="star-item on"></span>
-                  <span class="star-item on"></span>
-                  <span class="star-item on"></span>
-                  <span class="star-item half"></span>
-                  <span class="star-item off"></span>
+                  <Star :score="shop.rating" :size="24"/>
                 </div>
                 <div class="rating_section">{{shop.rating}}</div>
                 <div class="order_section">月售{{shop.recent_order_num}}单</div>
@@ -66,7 +62,7 @@ import { mapState } from "vuex";
 export default {
   data() {
     return {
-      baseImgUrl: "https://fuss10.elemecdn.com"  /// 因为本地写的server是保存的,请求回来的是图片地址的一部分, 所以需要加上饿了么的图片baseImgUrl地址
+      baseImgUrl: "https://fuss10.elemecdn.com" /// 因为本地写的server是保存的,请求回来的是图片地址的一部分, 所以需要加上饿了么的图片baseImgUrl地址
     };
   },
   computed: {
@@ -75,7 +71,6 @@ export default {
 };
 </script>
 <style lang="stylus" rel="stylesheet/stylus" scoped>
-
 @import '../../common/stylus/mixins.styl'
 .shop_container
   margin-bottom 50px
@@ -140,54 +135,6 @@ export default {
             .shop_rating_order_left
               float left
               color #ff9a0d
-              .star // 2x图 3x图
-                float left
-                font-size 0
-                .star-item
-                  display inline-block
-                  background-repeat no-repeat
-                &.star-48
-                  .star-item
-                    width 20px
-                    height 20px
-                    margin-right 22px
-                    background-size 20px 20px
-                    &:last-child
-                      margin-right 0
-                    &.on
-                      bg-image('./images/stars/star48_on')
-                    &.half
-                      bg-image('./images/stars/star48_half')
-                    &.off
-                      bg-image('./images/stars/star48_off')
-                &.star-36
-                  .star-item
-                    width 15px
-                    height 15px
-                    margin-right 6px
-                    background-size 15px 15px
-                    &:last-child
-                      margin-right 0
-                    &.on
-                      bg-image('./images/stars/star36_on')
-                    &.half
-                      bg-image('./images/stars/star36_half')
-                    &.off
-                      bg-image('./images/stars/star36_off')
-                &.star-24
-                  .star-item
-                    width 10px
-                    height 10px
-                    margin-right 3px
-                    background-size 10px 10px
-                    &:last-child
-                      margin-right 0
-                    &.on
-                      bg-image('./images/stars/star24_on')
-                    &.half
-                      bg-image('./images/stars/star24_half')
-                    &.off
-                      bg-image('./images/stars/star24_off')
               .rating_section
                 float left
                 font-size 10px
--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import App from './App'
 import router from './router'
 import store from './store/index'
 import Header from './components/Header/Header'
+import Star from './components/Star/Star'
 
 // 注册全局组件
 Vue.component('Header', Header)
+Vue.component('Star', Star)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
